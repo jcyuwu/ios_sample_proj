05b11318d06c25cfbdea78ade4c610f0ed509256
JCY <jcy@JCYs-Mac.local>
2018-02-14 15:05:31 +0800

    scene table view controller

---
+++ ios_sample_proj/SceneTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  SceneTableViewController.h
+//  ios_sample_proj
+//
+//  Created by JCY on 2018/2/14.
+//  Copyright © 2018年 KyosJhong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneTableViewController : UITableViewController
+
+@end
