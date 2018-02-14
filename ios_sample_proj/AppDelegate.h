423d3ba8d78fec5c6c61a7e78926391216ad2b56
JCY <jcy@JCYs-Mac.local>
2018-02-14 11:54:46 +0800

    Initial Commit

---
+++ ios_sample_proj/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ios_sample_proj
+//
+//  Created by JCY on 2018/2/14.
+//  Copyright © 2018年 KyosJhong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ios_sample_proj/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ios_sample_proj
+//
+//  Created by JCY on 2018/2/14.
+//  Copyright © 2018年 KyosJhong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
